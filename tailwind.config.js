0142fb7ae10b18da942871139724b62a3e181789
PRONOB BISWAS <biswaspronob@outlook.com>
2023-12-20 23:21:03 +0600

    stucture done before the footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,10 @@ tailwind.config = {
             'service-bg1':"linear-gradient(rgba(0, 0, 0, 0.445),rgba(0, 0, 0, 0.604)),url(./images/service-image01.jpg)",
             'service-bg2':"linear-gradient(rgba(0, 0, 0, 0.445),rgba(0, 0, 0, 0.604)),url(./images/service-image02.png)",
             'service-bg3': "linear-gradient(rgba(0, 0, 0, 0.445),rgba(0, 0, 0, 0.604)),url(./images/service-image03.png)",
+            'about-bg': "linear-gradient(rgba(0, 0, 0, 0.445),rgba(0, 0, 0, 0.604)),url(./images/about-bg.jpg)",
+            'about-card01': "linear-gradient(rgba(0, 0, 0, 0.445),rgba(0, 0, 0, 0.604)),url(./images/about_image01.jpg)",
+            'about-card02': "linear-gradient(rgba(0, 0, 0, 0.445),rgba(0, 0, 0, 0.604)),url(./images/about_image02.png)",
+            'about-card03': "linear-gradient(rgba(0, 0, 0, 0.445),rgba(0, 0, 0, 0.604)),url(./images/about_image03.png)",
             'footer-texture': "url('/img/footer-texture.png')",
           }
       }
