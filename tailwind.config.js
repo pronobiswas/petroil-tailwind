275ff7f4ea6eb47edf70cd8f3cde2f37c23dbfaf
PRONOB BISWAS <biswaspronob@outlook.com>
2023-12-20 08:44:01 +0600

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,9 @@ tailwind.config = {
           },
           backgroundImage: {
             'banner-bg': "linear-gradient(rgba(0, 0, 0, 0.445),rgba(0, 0, 0, 0.604)), url('./images/banner-bg.jpg')",
+            'service-bg1':"linear-gradient(rgba(0, 0, 0, 0.445),rgba(0, 0, 0, 0.604)),url(./images/service-image01.jpg)",
+            'service-bg2':"linear-gradient(rgba(0, 0, 0, 0.445),rgba(0, 0, 0, 0.604)),url(./images/service-image02.png)",
+            'service-bg3': "linear-gradient(rgba(0, 0, 0, 0.445),rgba(0, 0, 0, 0.604)),url(./images/service-image03.png)",
             'footer-texture': "url('/img/footer-texture.png')",
           }
       }
